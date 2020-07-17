15bfb88e8e42fcf5c4895cb0804b297afcaf279f
AsharGit <32973667+AsharGit@users.noreply.github.com>
2020-07-17 13:33:27 -0500

    update

--- Source/DeepLearning_Lesson2/imageclassification.py
+++ Source/DeepLearning_Lesson2/imageclassification.py
@@ -6,23 +6,29 @@ from keras.layers import Dense
 from keras.utils import to_categorical
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-#display the first image in the training data
-
+# display the first image in the training data
 plt.imshow(train_images[0, :, :], cmap='gray')
 plt.title('Ground Truth : {}'.format(train_labels[0]))
 plt.show()
 
-#process the data
-#1. convert each image of shape 28*28 to 784 dimensional which will be fed to the network as a single feature
+# process the data
+# 1. convert each image of shape 28*28 to 784 dimensional which will be fed to the network as a single feature
 dimData = np.prod(train_images.shape[1:])
 train_data = train_images.reshape(train_images.shape[0], dimData)
 test_data = test_images.reshape(test_images.shape[0], dimData)
 
-#change the labels frominteger to one-hot encoding
+# #convert data to float and scale values between 0 and 1
+# train_data = train_data.astype('float')
+# test_data = test_data.astype('float')
+# #scale data
+# train_data /= 255.0
+# test_data /= 255.0
+
+# change the labels frominteger to one-hot encoding
 train_labels_one_hot = to_categorical(train_labels)
 test_labels_one_hot = to_categorical(test_labels)
 
-#creating network
+# creating network
 model = Sequential()
 model.add(Dense(512, activation='sigmoid', input_shape=(dimData,)))
 model.add(Dense(512, activation='sigmoid'))
@@ -32,7 +38,35 @@ model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 history = model.fit(train_data, train_labels_one_hot, batch_size=256, epochs=20, verbose=1,
-                   validation_data=(test_data, test_labels_one_hot))
+                    validation_data=(test_data, test_labels_one_hot))
 
 [test_loss, test_acc] = model.evaluate(test_data, test_labels_one_hot)
 print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
+
+# Plot image from the test data
+index = 2000
+plt.imshow(test_data[index].reshape(28, 28), cmap='gray')
+plt.title("Test number image")
+plt.show()
+
+# Predict the number in the test data
+predict = model.predict(test_data[index].reshape(1, 784))
+print("Predicted number: ", predict.argmax())
+
+# Model Accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='best')
+plt.show()
+
+# Model Loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='best')
+plt.show()
